648ddff57d2b6cb733e4e2161c7aa52d87489aed
tpessoa <36314680+tpessoa@users.noreply.github.com>
2021-01-26 16:02:02 +0000

    docker node & mongo

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # EU-platform
 Platform for hosting European Union (EU) activities
+
+
+node:
+    container_name: web-service-REST
+    build: ./server
+    ports:
+      - "8080:8080"
+    volumes:
+      - ./server:/app
---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// connect to MongoDB
+mongoose
+    .connect(
+        'mongodb://db:27017/eu-platform-db',
+        {   useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    )
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('hell tiago'));
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('Server running on port ' + port);
+})
