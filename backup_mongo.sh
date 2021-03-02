1affe103d7bb2c027caeec37530321bcdc09a90e
tpessoa <36314680+tpessoa@users.noreply.github.com>
2021-03-02 14:02:15 +0000

    backup / restore

---
+++ backup_mongo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ 
+TODAY=`date +%Y%m%d%H%M`
+BACKUP_DIR=/backups/
+
+DATABASE_NAME="eu-platform-db"
+ 
+mkdir -p ${BACKUP_DIR}/${TODAY}
+ 
+docker exec mongodb mongodump --db ${DATABASE_NAME} --out /${BACKUP_DIR}/${TODAY}/
+#mongodump -h <DATABASE_HOST> -d <DATABASE_NAME> -u <USERNAME> -p <PASSWRD> --out ${BACKUP_DIR}/${TODAY}/
+
---
+++ restore_mongo.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+BACKUP_DIR=.backups/mongodb
+BACKUP_DIR_INSIDE=backups/
+BACKUP_DATE=$1
+
+DATABASE_NAME="eu-platform-db"
+
+if [ ! -d "./${BACKUP_DIR}/${BACKUP_DATE}"  ]
+    then
+        echo "There is no backup dated ${BACKUP_DATE} on bakcup mongodb folders!"
+        exit 1
+fi
+
+if [ "$(docker ps -q -f name=mongodb)" ]
+    then
+        echo "Restoring mongodb ${BACKUP_DATE}..."
+        #docker exec mongodb mongorestore --db ${DATABASE_NAME} --drop /${BACKUP_DIR}/${BACKUP_DATE} 
+        docker exec -it mongodb mongorestore --drop /${BACKUP_DIR_INSIDE}/${BACKUP_DATE}
+        echo "Mongodb restore done!"
+    else
+		echo "Containers NOT running!"
+		exit 1
+fi
+
+echo "Exiting..."
+exit 0
